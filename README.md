89d1f8e0d556d40fb14188e3f0fe98ceaf1fd678
parvezshams38 <parvezshams38@gmail.com>
2025-01-24 19:53:32 +0100

    running version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # telegram_bot_tinyLlama
-Telegram bot with TinyLlama LLM 
+
+This is a homework as part of the "Data Science Survival Skills" course at FAU Erlangen Nürnberg.
+
+Here I have used telegram bot API to communicate with a telegram bot that I created. Once the message is received by the bot it is processed by TinyLlama LLM model. 
+
+I have run this in GoogleColab to get GPU acceleration. 
+
+The code is ready-to-use in GoogleColab or other hosting platforms online. You only need to change the TOKEN and thats all!
+
--- main.py
+++ main.py
@@ -1,42 +1,42 @@
+
+import torch
+from transformers import pipelinefrom 
 from typing import Final
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
-TOKEN:Final = '7224677229:AAEOCF8oYTWLnrGdQVhdUrb37Sr-fWCONTg'
-BOT_USERNAME:Final = "@Salparbot"
-
-import torch
-from transformers import pipeline
+TOKEN:Final = 'Your_token_from_FatherBot_in_telegram'
 
 pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
                 torch_dtype=torch.bfloat16, device_map="auto")
 
-async def start_command(update:Update, context:ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello and welcome to my bot!!")
-
-async def help_command(update:Update, context:ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Help!")
-
-async def custom_command(update:Update, context:ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("What is custom!!")
-
 def handle_response(question:str) -> str:
     messages = []
     messages.append({"role": "user", "content": question})
-    print(question)
+    print(question) ## to see in the terminal what user asked
     prompt = pipe.tokenizer.apply_chat_template(messages,
                                                 tokenize=False,
                                                 add_generation_prompt=True)
     outputs = pipe(prompt, max_new_tokens=100,
                   do_sample=True, temperature=0.8, top_k=50, top_p=0.95)
     response = outputs[0]['generated_text'].split('<|assistant|>')[-1].strip()
-    print(response)
+    
+    if not response.endswith(('.', '!', '?')):
+        last_punctuation = max(response.rfind('.'), response.rfind('!'), 
+                              response.rfind('?'))
+        if last_punctuation != -1:
+            response = response[:last_punctuation + 1]
+        else:
+            response += '.'  # Default to a period 
+    
+    print(response) ## to see what our LLM answers
     return response
 
+async def start_command(update:Update, context:ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello and welcome to my bot!!")
+
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    #message_type: str = update.message.chat.type
     text: str = update.message.text
-    #print(text)
     response: str = handle_response(text)
     await update.message.reply_text(response)
 
@@ -49,8 +49,6 @@ if __name__ == "__main__":
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start",start_command))
-    app.add_handler(CommandHandler("help",help_command))
-    
     app.add_handler(MessageHandler(filters.TEXT,handle_message))
     app.add_error_handler(error)
 
